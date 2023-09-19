a434c6195d1dd5c850e68466ac3c028b3b5ce2bb
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-18 20:24:15 -0600

    Examples

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ numbers = [1, 2, 3, 4, 5, 6, 7]
 new_numbers = [number + 2 for number in numbers]
 
 print(new_numbers)
+
+name = "Ahmed"
+
+new_name = [n + "b" for n in name]
+print(new_name)
+
+range_1 = range(1,5)
+
+new_range = [n * 2 for n in range_1]
+
+print(new_range)
